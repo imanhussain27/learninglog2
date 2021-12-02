3eaa28791cc3953cdb00a12fb3274e8de248a6ba
imanhussain27 <89548618+imanhussain27@users.noreply.github.com>
2021-12-02 17:07:26 -0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,13 +1,22 @@
-from django.urls import path
+"""learning_log URL Configuration
 
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
-app_name='MainApp'
-
-urlpatterns= [
-    path('',views.index,name='index'),
-    path('topics',views.topics,name='topics'),
-    path('topics/<int:topic_id>/',views.topic,name='topic'),
-    path('new_topic/',views.new_topic,name='new_topic')
-
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('MainApp.urls')),
 ]
