3e5fb55fcbeb06c3063c576559eac0b66e408fa9
imanhussain27 <89548618+imanhussain27@users.noreply.github.com>
2021-12-02 17:05:33 -0600

    Add files via upload

---
+++ myshell.py
@@ -0,0 +1,26 @@
+import os 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","learning_log.settings")
+
+import django
+django.setup()
+
+from MainApp.models import Topic, Entry
+
+
+#making sure our data is in the database, first we select * all from our topic to see if all of it is there
+topics = Topic.objects.all()
+
+for topic in topics: 
+    print(topic.id, topic)
+
+t=Topic.objects.get(id=1)
+
+print(t.text)
+print(t.date_added)
+
+entries=t.entry_set.all()
+
+for entry  in entries: 
+    print(entry)
+
+    
